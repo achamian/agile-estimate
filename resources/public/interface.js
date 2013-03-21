3b84fc2a42417e3c5f62f1c8c0865d63ec3879ae
Srihari Sriraman <srihari@c42.in>
2013-03-21 20:45:34 +0530

    Added sliders

---
+++ resources/public/interface.js
@@ -0,0 +1,24 @@
+$(function() {
+  var value = 100;
+  $(".velocity-slider").slider({
+    min: 0,
+    max: 100,
+    value: value,
+    animate: true,
+    orientation: "vertical",
+     slide: function( event, ui ) {
+      // update the text value here
+    }
+  });
+
+  $(".stories-slider").slider({
+    min: 0,
+    max: 100,
+    value: value,
+    animate: true,
+    orientation: "vertical",
+     slide: function( event, ui ) {
+      // update the text value here
+    }
+  });
+});
